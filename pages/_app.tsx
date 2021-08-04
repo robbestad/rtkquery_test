98aa49c7faf428e6452701bbf70f1d2a85ac3737
Sven Anders Robbestad <sven.anders.robbestad@nav.no>
2021-08-04 12:05:49 +0200

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { Provider } from 'react-redux'
 import type { AppProps } from 'next/app'
 
+import { setupStore } from '../src/state/store'
+
+const store = setupStore()
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return  <Provider store={store}><Component {...pageProps} /></Provider>
 }
 export default MyApp
